1550e6cbc9720666e2c4d4565bbad8dcabf5ff1c
Ryunosuke <rew.7cc.12.1f@gmail.com>
2019-09-22 16:11:34 +0900

    add debug.hpp

---
+++ hal/include/debug.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "hal_config.hpp"
+
+constexpr char endl[] = "\r\n";
+
+namespace hal {
+    class Debug {
+    public:
+        Debug() {
+            setbuf(stdout, NULL);
+        }
+
+        Debug &operator<<(const char *const str) {
+            printf("%s", str);
+            return *this;
+        }
+
+        Debug &operator<<(const char c) {
+            printf("%c", c);
+            return *this;
+        }
+
+        Debug &operator<<(const int val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const int8_t val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const uint8_t val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const int16_t val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const uint16_t val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const int32_t val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const uint32_t val) {
+            printf("%d", val);
+            return *this;
+        }
+
+        Debug &operator<<(const float val) {
+            printf("%f", val);
+            return *this;
+        }
+
+        Debug &operator<<(const std::string &str) {
+            return (*this) << str.c_str();
+        }
+    };
+}
